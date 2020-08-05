960a2d5359bb38915a399b02db06810cd0648325
kawasoe-syouta <d6hyj3dyr@gmail.com>
2020-08-05 16:28:23 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production? 
 
   private
-
+  
+  def production?
+    Rails.env.production?
+  end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == Rails.application.credentials[:basic_auth][:user] &&
@@ -10,8 +14,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def production?
-    Rails.env.production?
-  end
-  
 end
