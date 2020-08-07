6075bf664caf04f6854f39fa4c4d9199d632cec4
neibis-zrm <berun0d02a46@gmail.com>
2020-08-07 19:22:52 +0900

    add items model and around

---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ApplicationRecord
+  has_many :item_images
+  has_many :messages
+  belongs_to :sell_user, class_name: "User"
+  belongs_to :buy_user, class_name: "User"
+  belongs_to_active_hash :phase
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_to_pay
+  belongs_to :category
+end
