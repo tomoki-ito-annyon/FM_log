871735c51f4e7fb23fddfbbd5c88620a30e5ae02
neibis-zrm <berun0d02a46@gmail.com>
2020-08-07 19:39:48 +0900

    add validation in items model

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :delivery_to_pay
   belongs_to :category
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :delivery_days, presence: true
 end
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,3 +1,5 @@
 class ItemImage < ApplicationRecord
   belongs_to :item
+
+  validates :image, presence: true, uniquness: {scope: [:item, :image]}
 end
