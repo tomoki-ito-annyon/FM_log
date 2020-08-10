01776e201db06873d9233682fcb1e7e2aaecf600
neibis-zrm <berun0d02a46@gmail.com>
2020-08-10 18:57:59 +0900

    fixup! Merge branch 'front-sell_page' of https://github.com/kawasoe-syouta/FM_log into front-sell_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :sells, only: [:index,:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sells, only: [:index,:create]
 end
